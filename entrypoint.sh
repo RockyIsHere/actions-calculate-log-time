81a319ab6cefd273698acb4320dd18e3f8673bad
rocky.mld2016@gmail.com <rocky.mld2016@gmail.com>
2024-05-16 23:08:19 +0530

    v0.1.9

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,31 @@
 
 set -eo pipefail
 
-curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
-            -H "Accept: application/vnd.github.v3+json" \
-            https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID \
-            > run_details.json
+# Ensure that $GITHUB_RUN_ID and $GITHUB_REPOSITORY are provided
+if [ -z "$GITHUB_RUN_ID" ]; then
+    echo "Error: GITHUB_RUN_ID is not set"
+    exit 1
+fi
+
+if [ -z "$GITHUB_REPOSITORY" ]; then
+    echo "Error: GITHUB_REPOSITORY is not set"
+    exit 1
+fi
+
 # Fetch run details from GitHub Actions API and capture start time
+curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" \
+    > run_details.json
+
+# Extract start time from run details
 start_at=$(jq -r '.created_at' run_details.json)
+
+# Convert start time to Unix timestamp
 start_time=$(date -d "$start_at" +%s)
 echo "Start Time: $(date -d "$start_at" +%s)"
+
+# Output end time (current time)
 echo "End Time: $(date +%s)"
 
 # Function to calculate and output duration
@@ -19,21 +36,20 @@ calculate_duration() {
 
     # Convert duration to minutes and hours if needed
     if [ "$duration" -lt 60 ]; then
-        echo "duration=${duration}s" >> $GITHUB_OUTPUT
+        echo "duration=${duration}s"
     else
         local duration_minutes=$((duration / 60))
         if [ "$duration_minutes" -lt 60 ]; then
-            echo "duration=${duration_minutes}m" >> $GITHUB_OUTPUT
+            echo "duration=${duration_minutes}m"
         else
             local duration_hours=$((duration_minutes / 60))
             local duration_remaining_minutes=$((duration_minutes % 60))
-            echo "duration=${duration_hours}h ${duration_remaining_minutes}m" >> $GITHUB_OUTPUT
+            echo "duration=${duration_hours}h ${duration_remaining_minutes}m"
         fi
     fi
 }
 
 # Call function to calculate and output duration
 calculate_duration
-
 
 echo "[+] Start Time - Done"
