e4d2edbb8b731ac9b9ddc288b92da52d39173cea
rocky.mld2016@gmail.com <rocky.mld2016@gmail.com>
2024-05-16 14:44:27 +0530

    v0.0.3

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,13 @@
 set -eo pipefail
 
 start(){
-    echo "start_time=$(date +%s)" >> $GITHUB_OUTPUT
+    echo "run_id=$(echo $GITHUB_RUN_ID)" >> $GITHUB_OUTPUT
+    curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/${{ github.repository }}/actions/runs/$(echo $GITHUB_RUN_ID) \
+    > run_details.json
+    start_at=$(jq -r '.created_at' run_details.json)
+    echo "start_time=$(start_at)" >> $GITHUB_OUTPUT
 }
 
 start
