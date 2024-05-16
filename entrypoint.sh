6ca88fcea3d43b6879fb43c24f3ca3ca9d70c2f2
rocky.mld2016@gmail.com <rocky.mld2016@gmail.com>
2024-05-16 14:50:05 +0530

    v0.0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,19 @@
 set -eo pipefail
 
 start(){
-    echo "run_id=$(echo $GITHUB_RUN_ID)" >> $GITHUB_OUTPUT
-    curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+    # Fetch run details and store in run_details.json
+    curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/${{ github.repository }}/actions/runs/$(echo $GITHUB_RUN_ID) \
+    "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" \
     > run_details.json
+
+    # Extract start time from run_details.json
     start_at=$(jq -r '.created_at' run_details.json)
-    echo "start_time=$(start_at)" >> $GITHUB_OUTPUT
+
+    # Output start time to the specified output file
+    echo "start_time=$start_at" >> "$GITHUB_OUTPUT"
 }
 
 start
 
-echo "[+] Start Time - Working"
+echo "[+] Start Time - Working" # You can put whatever message you want here
